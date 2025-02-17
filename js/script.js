4b71799ba6f2204162f41dfac43a90cc8d0354c3
101727 <101727@glr.nl>
2025-02-17 23:58:36 +0000

    5th commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function veranderTitel() {
+    document.getElementById("title").innerText = "Welkom op mijn creatieve ruimte!";
+}
+
+function toonBericht() {
+    alert("Bedankt voor het bezoeken van mijn website!");
+}
